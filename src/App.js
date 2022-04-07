2f266129d3663de900d1cc89cf16ac40ae8e6ed4
Neamul01 <neamulhaque2002@gmail.com>
2022-04-07 15:15:00 +0600

    form added and authentication added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import './App.css';
-import { getAuth } from "firebase/auth";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from './firebase.init';
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 
 const auth = getAuth(app);
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('')
+
+
+  const handleEmailBlur = e => {
+    setEmail(e.target.value)
+  }
+  const handlePassBlur = e => {
+    setPass(e.target.value)
+  }
+
+  const handleFormSubmit = e => {
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth, email, pass)
+      .then((userCredential) => {
+        const user = userCredential.user
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   return (
-    <div className="App">
-      <form>
-        <input type="text" name="" id="" />
-        <input type="password" name="" id="" />
-      </form>
+    <div>
+      <div className="register w-50 mx-auto">
+        <h2 className="text-primary my-3">Register Now!!!</h2>
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePassBlur} type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
